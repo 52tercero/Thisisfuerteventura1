936dc23d9e2c2ea28ff695482f1bd903f50dc9b1
52tercero <52tercero@gmail.com>
2025-11-09 14:20:50 +0000

    Add warm function to allow scheduled ping (SITE_URL env) for keeping aggregate warm

---
+++ netlify/functions/warm.js
@@ -0,0 +1,12 @@
+﻿exports.handler = async () => {
+  try {
+    const site = process.env.SITE_URL || '';
+    if (site) {
+      const url = site.replace(/\/$/, '') + '/.netlify/functions/aggregate?dedupe=1';
+      await fetch(url, { headers: { 'User-Agent': 'WarmFunction/1.0' } }).catch(() => {});
+    }
+    return { statusCode: 204, body: '' };
+  } catch (e) {
+    return { statusCode: 200, body: '' };
+  }
+};
