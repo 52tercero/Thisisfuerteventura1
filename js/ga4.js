00c960538553a085182d48735f5a74fd88298b06
52tercero <52tercero@gmail.com>
2025-12-10 01:04:29 +0000

    Add GA4 Measurement ID G-46FPEKQGKZ via CSP-safe external loader js/ga4.js across main pages

---
+++ js/ga4.js
@@ -0,0 +1,14 @@
+﻿// Load GA4 gtag without inline code and configure measurement ID
+(function(){
+  try {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ window.dataLayer.push(arguments); }
+    var s = document.createElement('script');
+    s.async = true;
+    s.src = 'https://www.googletagmanager.com/gtag/js?id=G-46FPEKQGKZ';
+    var ref = document.getElementsByTagName('script')[0];
+    ref.parentNode.insertBefore(s, ref);
+    gtag('js', new Date());
+    gtag('config', 'G-46FPEKQGKZ');
+  } catch (e) { /* noop */ }
+})();
